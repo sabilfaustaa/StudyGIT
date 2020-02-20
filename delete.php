a708d5a1a87fdb92b72b7c6a363272f9b8a9357d
Muhamad Sabil Fausta <msabilfaustaa@gmail.com>
2020-02-20 14:32:21 +0700

    Commit Study Cases

---
+++ delete.php
@@ -0,0 +1,15 @@
+<?php 
+	$hapus = 'Hapus data ?';
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<span><?= $hapus ?></span>
+		<button>Ya</button>
+		<button>Tidak</button>
+	</body>
+</html>
---
+++ tambah.php
@@ -0,0 +1,15 @@
+<?php
+	$tambah;
+	$tambah = "Muhamad Sabil Fausta";
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Muhamad Sabil Fausta</title>
+	</head>
+	<body>
+		<h1><?= $tambah ?></h1>
+		<p>Eleven Software Engineering 2</p>
+	</body>
+</html>
